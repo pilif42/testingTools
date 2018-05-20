0a91c5f67d3861c145616d19e99b5f038a4fefb1
Philippe Brossier <brossierp@gmail.com>
2018-05-20 14:17:00 +0100

    tidy up on multi threading

--- src/main/java/com/mysample/service/ThreadSafeService.java
+++ src/main/java/com/mysample/service/ThreadSafeService.java
@@ -1,13 +1,43 @@
 package com.mysample.service;
 
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Several options to make it thread-safe:
+ *
+ * Option 1 with synchronized
+ * Option 2 with a Lock:ReentrantLock, ReadWriteLock, StampedLock
+ * Option 3 with a Semaphore
+ */
 public class ThreadSafeService {
-    int count = 0;
+
+    private int count = 0;
+
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getCount() {
         return count;
     }
 
-    synchronized public void increment() {
-        count = count + 1;
+    /**
+     * Option 1
+     *
+     * synchronized public void increment() {
+     * count = count + 1;
+     * }
+     */
+
+    /**
+     * Option 2
+     */
+    public void increment() {
+        lock.lock();
+
+        try {
+            count = count + 1;
+        } finally {
+            // important to wrap your code into a try/finally block to ensure unlocking in case of exceptions.
+            lock.unlock();
+        }
     }
 }
--- src/test/java/com/mysample/service/NonThreadSafeServiceTest.java
+++ src/test/java/com/mysample/service/NonThreadSafeServiceTest.java
@@ -1,5 +1,6 @@
 package com.mysample.service;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import java.util.concurrent.*;
 import static org.junit.Assert.assertEquals;
 
 /**
- * The service NonThreadSafeService is on purpose NOT thread safe. The test below proves it.
+ * The service NonThreadSafeService is on purpose NOT thread safe. The test below proves it. Remove @Ignore for manual run only.
  *
  * Instead of always having a final count of NUMBER_OF_TASKS_TO_RUN, the actual result varies with every execution.
  * The reason is that we share a mutable variable upon different threads without synchronizing the access to this variable
@@ -20,7 +21,7 @@ public class NonThreadSafeServiceTest {
     private final static int THREAD_POOL_SIZE = 5;
     private final static int NUMBER_OF_TASKS_TO_RUN = 1000000;
 
-    @Test
+    @Test @Ignore
     public void testIncrement() throws InterruptedException, ExecutionException {
         long startingTime = System.currentTimeMillis();
 
